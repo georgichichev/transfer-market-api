ad2ccdcdea1a4c1e9e0faaf86707aba1e18f8668
Georgi <georgichichev15@Gmail.com>
2022-07-05 18:12:02 +0300

    Add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {mongoose} = require('mongoose');
 const app = express();
 const routes = require('./src/routes.js');
+const cors = require('./src/middlewares/cors.js');
 
 const startDataBase = () =>{
     try {
@@ -17,6 +18,7 @@ const startDataBase = () =>{
 startDataBase();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3000, () => console.log('Server is listening on port 3000...'))
