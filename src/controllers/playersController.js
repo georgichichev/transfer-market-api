ba1aaba457db51ad36c46b72b664a173fb5362d7
Georgi <georgichichev15@Gmail.com>
2022-07-05 18:12:39 +0300

    Add create and get one player

--- src/controllers/playersController.js
+++ src/controllers/playersController.js
@@ -5,6 +5,23 @@ router.get('/', async (req, res) => {
     const players = await playerService.getAllPlayers();
 
     res.json(players);
+});
+
+router.post('/create', async (req, res) => {
+   const player = await playerService.createPlayer(req.body);
+
+   res.json(player);
+});
+
+router.get('/:id', async(req, res) => {
+    try{
+        const player = await playerService.getOnePlayer(req.params.id);
+
+        res.json(player);
+    }
+    catch (err){
+        res.status(400).json({message: 'Player not found.'})
+    }
 })
 
 module.exports = router;
--- src/services/playerService.js
+++ src/services/playerService.js
@@ -3,3 +3,14 @@ const {Player} = require('../models/Player.js');
 exports.getAllPlayers = () =>{
     return Player.find();
 };
+
+exports.createPlayer = async (data) =>{
+    const player = await Player.create(data);
+
+    return player
+};
+exports.getOnePlayer = async (id) =>{
+    const player = await Player.findById(id);
+
+    return player
+}
