27fcb92e0847cf78ff964e2d8a93c7db6e2bf6d3
Georgi <georgichichev15@Gmail.com>
2022-07-05 17:37:04 +0300

    Add routes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
+const routes = require('./src/routes.js');
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => console.log('Server is listening on port 3000...'))
