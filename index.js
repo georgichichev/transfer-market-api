dd90e1d46c1449e8f8e33600bc480268726b2a63
Georgi <georgichichev15@Gmail.com>
2022-07-05 17:48:21 +0300

    Connect to database

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const {mongoose} = require('mongoose');
 const app = express();
 const routes = require('./src/routes.js');
+
+const startDataBase = () =>{
+    try {
+        mongoose.connect('mongodb://localhost:27017/transfer-market', () =>{
+            console.log('Database connected.')
+        })
+    }
+    catch (err){
+        console.log('Error connecting to the database.')
+    }
+};
+
+startDataBase();
 
 app.use(express.json());
 app.use(routes);
