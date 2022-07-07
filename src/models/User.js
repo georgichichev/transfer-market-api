842a8eaeb7658d1f1f3750b95a8b73a85c65da6c
Georgi <georgichichev15@Gmail.com>
2022-07-07 16:11:00 +0300

    Add email validator in mongoose

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,18 @@ const {mongoose} = require('mongoose');
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
+        validate: {
+            validator: function(email) {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+            },
+            message: 'Please enter a valid email address!'
+        },
         required: true
     },
     username:{
         type: String,
+        minLength: [4, 'Username must be at least 4 characters long.'],
+        maxLength: [20, 'Username cannot be more than 20 characters long.'],
         required: true
     },
     password:{
